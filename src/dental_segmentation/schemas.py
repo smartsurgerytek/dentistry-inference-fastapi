3572c083cd2d3dc342b9510b95eaf94dc8c0233f
BoAn <fluttering13@gmail.com>
2024-12-05 11:41:51 +0800

    feat: add new validation for yolov8 label first element check

--- src/dental_segmentation/schemas.py
+++ src/dental_segmentation/schemas.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel
-from typing import List, Dict, Union
-from pydantic import BaseModel
-
+from typing import List, Dict, Union, Any
+from pydantic import BaseModel, validator
 
 # define yolo segmentation model
 class YoloSegmentation(BaseModel):
     color_dict: Dict[int,List[int]] # class ID and color RGB
     class_names: Dict[int, str] # class ID and class name
-    yolov8_contents: List[Union[int, float]] # List can contain both int and float
-
+    yolov8_contents: List[List[Union[int, float]]] # List can contain both int and float
+    @validator('yolov8_contents')
+    def validate_contents(cls, contents):
+        for yolov8_label_list in contents:
+            if not isinstance(yolov8_label_list[0], int):
+                raise ValueError("yolov8_label_list first element is class ID must be fint")
+        return contents    
 # define yolo response
 class YoloSegmentationResponse(BaseModel):
     request_id: int  # 圖像的唯一標識符
