964b5e4bd636824856227c55ff2b921e094fc295
LianChia <s20s20wuxing@gmail.com>
2024-12-23 14:00:01 +0800

    feat：在DentalMeasurements新增Field 增加型別驗證與描述、更改stage定義

--- src/dental_measure/schemas.py
+++ src/dental_measure/schemas.py
@@ -2,18 +2,21 @@ from pydantic import BaseModel, validator
 from typing import List, Tuple, Any
 from fastapi import FastAPI, HTTPException
 from typing import Annotated
+from typing import Union, Tuple
+from pydantic import BaseModel, Field
+from typing_extensions import Literal
 import ast
 
     
 class DentalMeasurements(BaseModel):
-    side_id: int
+    side_id: int = Field(..., ge=0, description="Side ID must be a non-negative integer")
     CEJ: Tuple[int, int]
     ALC: Tuple[int, int]
     APEX: Tuple[int, int]
-    CAL: float
-    TRL: float
-    ABLD: float
-    stage: str
+    CAL: float = Field(..., ge=0, description="CAL must be a non-negative float")
+    TRL: float = Field(..., ge=0, description="TRL must be a non-negative float")
+    ABLD: float = Field(..., ge=0, description="ABLD must be a non-negative float")
+    stage: Union[Literal[0, 1, 2, 3, "i", "ii", "iii"]]
 
 class Measurements(BaseModel):
     teeth_id: int
