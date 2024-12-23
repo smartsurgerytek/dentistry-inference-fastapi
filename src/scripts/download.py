8ac862d1268330aa9824a71d81a08c0809309612
boan <julie@smartsurgery.com>
2024-12-23 16:50:00 +0800

    fix: files name to download

--- src/scripts/download.py
+++ src/scripts/download.py
@@ -6,8 +6,8 @@ filename = "dental_contour/dentistry_yolov11n-seg_contour4.46.pt"
 
 os.makedirs('./models',exist_ok=True)
 save_map={
-    "dental_contour/dentistryContour_yolov11n_4.46.pt":"./models/dentistryContour_yolov11n_4.46.pt",
-    "all_category/dentistry_yolov11x-seg_4.42.pt":"./models/dentistry_yolov11x-seg_4.42.pt"}
+    "dental_contour/dentistryContour_yolov11n-seg_4.46.pt":"./models/dentistryContour_yolov11n_4.46.pt",
+    "all_category/dentistry_yolov11x-seg-all_4.42.pt":"./models/dentistry_yolov11x-seg_4.42.pt"}
 for filename, save_path in save_map.items():
     file_path = hf_hub_download(repo_id=repo_id, filename=filename)
     shutil.copy(file_path, save_path)
