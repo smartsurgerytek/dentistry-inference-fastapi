f46a69216442eb72662a305a839ee391e4ea4f9d
boan <julie@smartsurgery.com>
2024-12-25 18:05:45 +0800

    test: add unit test case

--- tests/unit/test_dentalMeasure.py
+++ tests/unit/test_dentalMeasure.py
@@ -2,19 +2,20 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 import cv2
-from src.allocation.domain.dental_measure.main import *
-def test_dentalEstimation_normalImage():
-    image=cv2.imread('./data/pics/nomal-x-ray-0.8510638-270-740_0_2022011008.png')
-    if image is None:
-        raise ValueError("Image not found, check test image path (or utf8 problems) or cv2 package")
-    results_list=dental_estimation(image, scale=(31/960,41/1080), return_type='dict')
-    if not results_list:
-        raise ValueError("When input normal image, the result should be found, plz check dental_estimation function")
+from src.allocation.domain.dental_segmentation.main import *
 
-def test_dentalEstimation_blackimage():
-    image=cv2.imread('./data/pics/black.png')
+def test_dentalSegmentation_normalImage():
+    image=cv2.imread('./tests/files/nomal-x-ray-0.8510638-270-740_0_2022011008.png')
     if image is None:
         raise ValueError("Image not found, check test image path (or utf8 problems) or cv2 package")
-    results_list=dental_estimation(image, scale=(31/960,41/1080), return_type='dict')
-    if results_list:
-        raise ValueError("When input black image, the result should be [], plz check dental_estimation function")
+    results_dict=yolo_transform(image, return_type='dict')
+    if not results_dict.get('yolov8_contents'):
+        raise ValueError("When input normal image, the result should be found, plz check dental_segmentation function")
+
+def test_dentalSegmentation_blackimage():
+    image=cv2.imread('./tests/files/black.png')
+    if image is None:
+        raise ValueError("Image not found, check test image path (or utf8 problems) or cv2 package")
+    results_dict=yolo_transform(image, return_type='dict')
+    if results_dict.get('yolov8_contents'):
+        raise ValueError("When input black image, the result should be [], plz check dental_segmentation function")
