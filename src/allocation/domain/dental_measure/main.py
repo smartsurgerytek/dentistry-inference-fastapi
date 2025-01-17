c16e3b18a0d44a143efc6db24d6234625c5a8e69
boan <boan.tsai@smartsurgerytek.com>
2025-01-17 15:17:23 +0800

    fix: the path of contour model

--- src/allocation/domain/dental_measure/main.py
+++ src/allocation/domain/dental_measure/main.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import numpy as np
 from src.allocation.domain.dental_measure.utils import *
 components_model=YOLO('./models/dentistry_yolov11x-seg-all_4.42.pt')
-contour_model=YOLO('./models/dentistryContour_yolov11n_4.46.pt')
+contour_model=YOLO('./models/dentistryContour_yolov11n-seg_4.46.pt')
 
 def extract_features(masks_dict, original_img):
     """從遮罩中提取特徵點與區域資訊"""
