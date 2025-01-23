61d9a608d27c7963d63491b5d74825913adefc9d
BoAn-influxfin <boan@influxfin.com>
2025-01-23 11:37:30 +0800

    fix: model is a parameter of input

--- tests/unit/test_dentalSegmentation.py
+++ tests/unit/test_dentalSegmentation.py
@@ -4,15 +4,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'
 import cv2
 from src.allocation.domain.dental_segmentation.main import *
 import yaml
+model=YOLO('./models/dentistry_yolov11x-seg-all_4.42.pt')
 def test_dentalSegmentation_normalImage():
     image=cv2.imread('./tests/files/nomal-x-ray-0.8510638-270-740_0_2022011008.png')
     if image is None:
         raise ValueError("Image not found, check test image path (or utf8 problems) or cv2 package")
-    results_dict=yolo_transform(image, return_type='dict')
+    results_dict=yolo_transform(image, model, return_type='dict')
 
     with open('./conf/dentistry_PA.yaml', 'r') as file:
         config=yaml.safe_load(file)
-    image=yolo_transform(image, return_type='image', config=config)
+    image=yolo_transform(image, model, return_type='image', config=config)
     
     if not results_dict.get('yolov8_contents'):
         raise ValueError("When input normal image, the result should be found, plz check dental_segmentation function")
@@ -22,7 +23,7 @@ def test_dentalSegmentation_blackimage():
     image=cv2.imread('./tests/files/black.png')
     if image is None:
         raise ValueError("Image not found, check test image path (or utf8 problems) or cv2 package")
-    results_dict=yolo_transform(image, return_type='dict')
+    results_dict=yolo_transform(image, model, return_type='dict')
     if results_dict.get('yolov8_contents'):
         raise ValueError("When input black image, the result should be [], plz check dental_segmentation function") 
     
