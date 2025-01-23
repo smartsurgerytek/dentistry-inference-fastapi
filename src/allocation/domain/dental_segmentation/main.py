59e482af83a8bca645f60bc8bdc94907b703fcd4
BoAn-influxfin <boan@influxfin.com>
2025-01-23 11:10:02 +0800

    refactor: to generalize using of yolo_transform for PA, PANO

--- src/allocation/domain/dental_segmentation/main.py
+++ src/allocation/domain/dental_segmentation/main.py
@@ -4,11 +4,11 @@ import cv2
 import matplotlib.pyplot as plt
 from skimage.measure import label, regionprops, find_contours
 from skimage.measure import approximate_polygon
-# import os 
-# import sys
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+import os 
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
 from src.allocation.domain.dental_segmentation.utils import *
-
+import yaml
 
 model=YOLO('./models/dentistry_yolov11x-seg-all_4.42.pt')
 def find_center_mask(mask_binary):
@@ -47,48 +47,16 @@ def get_yolov8_label(mask_binary,tolerance=0.5):
         points.extend(merged)
     return points
 
-def yolo_transform(image, return_type='dict'):
+def yolo_transform(image, return_type='dict', config=None):
+    if return_type == 'image' and config is None:
+        raise ValueError("Provide a config for segmentation colors when return_type is 'image")
+
     # 定義文本參數
-    image= cv2.resize(image, (1280,960))
+    color_list=config['color_list']
+    #color_list=[[color[2],color[1],color[0]] for color in color_list]
     plot_image=image.copy()
-    color_list = [
-        [0, 240, 255],
-        [65, 127, 0],
-        [0, 0, 255],
-        [113, 41, 29],
-        [122, 21, 135],
-        [0, 148, 242],
-        [4, 84, 234],
-        [0, 208, 178],
-        [52, 97, 148],
-        [121, 121, 121],
-        [212, 149, 27],
-        [206, 171, 255],
-        [110, 28, 216]
-    ]
-
-    color_list=[[color[2],color[1],color[0]] for color in color_list]
     color_dict = {i: color for i, color in enumerate(color_list)}
-    text_dict={
-    0: "Alveolar_bone",
-    1: "Caries",
-    2: "Crown",
-    3: "Dentin",
-    4: "Enamel",
-    5: "Implant",
-    6: "Mandibular_alveolar_nerve",
-    7: "Maxillary_sinus",
-    8: "Periapical_lesion",
-    9: "Post_and_core",
-    10: "Pulp",
-    11: "Restoration",
-    12: "Root_canal_filling"
-    }
 
-    # image_path = './M_02172016084007_0000000S1555078C_2_001_001-003-1 cutX2_001539000.jpg'
-    # image = cv2.imread(image_path)
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
     results = model(image)
 
     # 獲取類別名稱
@@ -98,7 +66,6 @@ def yolo_transform(image, return_type='dict'):
     for result in results:
         boxes = result.boxes  # Boxes object for bbox outputs
         masks = result.masks  # Masks object for segmentation masks outputs
-        
         if masks is None:
             continue
         predict_label=[]
@@ -129,14 +96,14 @@ def yolo_transform(image, return_type='dict'):
             
             # Apply mask to the original image
             mask_colored = np.zeros((mask_binary.shape[0], mask_binary.shape[1], 3), dtype=np.uint8)
-            if class_name != 'Background':
+            if class_name != 'Background' and return_type=='image':
                 mask_colored[mask_binary == 255] = color_dict[class_id]
                 # Overlay the colored mask
                 plot_image = cv2.addWeighted(plot_image, 1, mask_colored, 0.8, 0)
                 
                 
     if return_type=='image':
-        label_image=get_label_text_img(result.boxes.cls.cpu().numpy().astype(int), plot_image.shape[1], color_dict, text_dict)
+        label_image=get_label_text_img(result.boxes.cls.cpu().numpy().astype(int), plot_image.shape[1], color_dict, class_names)
 
         plot_image=np.concatenate((plot_image, label_image), axis=0)
 
@@ -159,6 +126,8 @@ def show_plot(image):
 
 
 if __name__=='__main__':
-    image=cv2.imread('./tests/nomal-x-ray-0.8510638-270-740_0_2022011008.png')
-    tests=yolo_transform(image, return_type='dict')
+    image=cv2.imread('./tests/files/nomal-x-ray-0.8510638-270-740_0_2022011008.png')
+    with open('./conf/dentistry_PA.yaml', 'r') as file:
+        config=yaml.safe_load(file)
+    tests=yolo_transform(image, return_type='image', config=config)
     #show_plot(result)
