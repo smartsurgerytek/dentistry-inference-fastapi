b0e73d2439e083d812b569ff6dd907d8547fd8c5
BoAn <fluttering13@gmail.com>
2025-04-01 12:08:55 +0800

    refactor: model_names

--- src/allocation/service_layer/download.py
+++ src/allocation/service_layer/download.py
@@ -2,15 +2,14 @@ from huggingface_hub import hf_hub_download
 import shutil
 import os
 repo_id = "smartsurgery/dentistry-models"  # repo 名稱
-filename = "dental_contour/dentistry_yolov11n-seg_contour4.46.pt"
 
 os.makedirs('./models',exist_ok=True)
 save_map={
-    "PA_dental_contour/dentistryContour_yolov11n-seg_4.46.pt":"./models/dentistryContour_yolov11n-seg_4.46.pt",
-    "PA_segmentation/dentistry_yolov11x-seg-all_4.42.pt":"./models/dentistry_yolov11x-seg-all_4.42.pt",
-    "PANO_caries_detection/dentistry_pano-caries-detection-resNetFpn_5.12.pth":"models/dentistry_pano-caries-detection-resNetFpn_5.12.pth",
-    "PA_PANO_classification/pa_pano_classification.pth":"models/pa_pano_classification.pth",
-    "PANO_fdi_segmentation/pano_fdi_segmentation_25.12.pt":'models/pano_fdi_segmentation_25.12.pt'
+    "PA_dental_contour/dentistry_pa-contour_yolov11n-seg_24.46.pt":"./models/dentistry_pa-contour_yolov11n-seg_24.46.pt",
+    "PA_segmentation/dentistry_pa-segmentation_yolov11x-seg-all_24.42.pt":"./models/dentistry_pa-segmentation_yolov11x-seg-all_24.42.pt",
+    "PANO_caries_detection/dentistry_pano-CariesDetection_resNetFpn_25.12.pth":"models/dentistry_pano-CariesDetection_resNetFpn_25.12.pth",
+    "PA_PANO_classification/dentistry_pa-pano-classification_fpcl_25.10.pth":"models/dentistry_pa-pano-classification_fpcl_25.10.pth",
+    "PANO_fdi_segmentation/dentistry_pano-fdi_segmentation_yolo11x-seg_25.12.pt":'models/dentistry_pano-fdi_segmentation_yolo11x-seg_25.12.pt'
     }
 
 for filename, save_path in save_map.items():
